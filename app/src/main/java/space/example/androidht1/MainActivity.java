8ff2f1241aca890f0014393807c8f076ad52a330
Rustam16 <muhambetovrustam1@gmail.com>
2021-03-01 00:16:26 +0600

    Initial commit

---
+++ app/src/main/java/space/example/androidht1/MainActivity.java
@@ -0,0 +1,46 @@
+package space.example.androidht1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button buttonApplication;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        buttonApplication = findViewById(R.id.btnApplication);
+
+        EditText editText = findViewById(R.id.editText);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND){
+                    handled =true;
+                }
+                return handled;
+            }
+        });
+
+        buttonApplication.setOnClickListener(v -> {
+           String query = editText.getText().toString();
+           Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+           intent.putExtra("query",query);
+           startActivity(intent);
+        });
+    }
+}
